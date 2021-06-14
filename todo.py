96dc76057b736376643c3b4749f03929bad4fc8b
Metin <metinkonuk@hotmail.com>
2021-06-14 05:40:52 +0300

    Update todo.py

--- todo.py
+++ todo.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/metinkonuk/Desktop/TodoApp/todo.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////todo.db'
 db = SQLAlchemy(app)
 
 @app.route("/")
